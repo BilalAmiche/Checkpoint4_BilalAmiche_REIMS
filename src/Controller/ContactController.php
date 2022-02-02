45bf982063ed37a2a95d28ced480d2c8a1c53185
Bilal Amiche <bilal-amiche_student2021@wilder.school>
2022-02-02 19:31:53 +0100

    create the ContactForm

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,16 +3,35 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ContactType;
+use App\Entity\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer)
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
+            
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('ton@gmail.com')
+                ->subject('vous avez reçu unn email')
+                ->text('Sender : '.$contactFormData['email'].\PHP_EOL.
+                    $contactFormData['Message'],
+                    'text/plain');
+            $mailer->send($message);
+            $this->addFlash('success', 'Vore message a été envoyé');
+        }
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'CContactController',
+            'form' => $form,
         ]);
     }
 }
