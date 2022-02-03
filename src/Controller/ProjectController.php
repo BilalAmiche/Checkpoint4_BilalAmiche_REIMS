84667efce074062003e7a74107e7d48484f13823
Bilal Amiche <bilal-amiche_student2021@wilder.school>
2022-02-03 12:19:03 +0100

    update the Skills Fixtures

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProjectRepository;
 
 class ProjectController extends AbstractController
 {
     #[Route('/project', name: 'project')]
-    public function index(): Response
+    public function index(ProjectRepository $projectRepository): Response
     {
+        $projects = $projectRepository->findAll();
+
         return $this->render('project/index.html.twig', [
-            'controller_name' => 'ProjectController',
+            'projects' => $projects,
         ]);
+    }
+
+    #[Route('/project/{id}', name: 'project_show')]
+    public function show(Project $project): Response
+    {
+        return $this->render('project/show.html.twig', ['project' => $project]);
     }
 }
--- src/DataFixtures/SkillsFixtures.php
+++ src/DataFixtures/SkillsFixtures.php
@@ -4,14 +4,41 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Skills;
 
 class SkillsFixtures extends Fixture
 {
+    public const SKILLSS = [
+        [
+            "name" => "PHP",
+            "description" => "test",
+        ],
+
+        [
+            "name" => "HTML/CSS",
+            "description" => "test",
+        ],
+
+        [
+            "name" => "SQL",
+            "description" => "test",
+        ],
+
+        [
+            "name" => "Symfony",
+            "description" => "test",
+        ],
+    ];
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        foreach (self::SKILLSS as $data) {
+            $skills = new Skills();
+            $skills->setName($data['name']);
+            $skills->setDescription($data['description']);
+            $manager->persist($skills);
+        }
         $manager->flush();
-    }
+    }    
 }
+
