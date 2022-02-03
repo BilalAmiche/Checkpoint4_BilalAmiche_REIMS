9d29e7ce5e329b607b4b2646186ac8bc5bf7b8d4
Bilal Amiche <bilal-amiche_student2021@wilder.school>
2022-02-03 14:49:05 +0100

    init the CRUD for project

--- src/Controller/CVController.php
+++ src/Controller/CVController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Skills;
+use App\Repository\SkillsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class CVController extends AbstractController
 {
     #[Route('/cv', name: 'cv')]
-    public function index(): Response
+    public function index(SkillsRepository $skillsRepository): Response
     {
+        $skillss = $skillsRepository->findAll();
+
         return $this->render('cv/index.html.twig', [
-            'controller_name' => 'CVController',
+            'skillss' => $skillss,
         ]);
     }
 }
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -7,6 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProjectRepository;
+use Doctrine\DBAL\Types\DateType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormTypeInterface;
+use App\Form\ProjectType;
 
 class ProjectController extends AbstractController
 {
@@ -17,6 +24,24 @@ class ProjectController extends AbstractController
 
         return $this->render('project/index.html.twig', [
             'projects' => $projects,
+        ]);
+    }
+
+    #[Route('/project/new', name: 'project_new')]
+    public function createAction(Request $request, EntityManagerInterface $entityManager) : Response
+    {
+        $project = new Project();
+        $form = $this->createForm(ProjectType::class, $project);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+          $entityManager->persist($project);
+          $entityManager->flush();
+          return $this->redirectToRoute('project_index');
+        }
+        return $this->render('project/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
